fbf309d41cf9f781f2d21707f1cc260140ac9bc9
GiaSen <giammasena@gmail.com>
2020-09-11 16:16:35 +0200

    nx609j: copy vndk-29 libbinderthreadstate.so to vendor
    09-11 14:55:24.310  1517  1517 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gx_fpd": library "libbinderthreadstate.so" not found: needed by /vendor/lib64/libbindergx.so in namespace (default)
    
    Change-Id: I9e6203d23a0f5b14168042c363232938b3f418fb

--- device.mk
+++ device.mk
@@ -80,6 +80,9 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     libpowermanager.vendor
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbinderthreadstate.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinderthreadstate.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     WifiOverlay_nx609j
