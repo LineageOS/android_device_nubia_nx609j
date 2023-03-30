e714a6d4aaad645735d0f16a382e41db1935724c
Cyborg2017 <wangyang0987654@gmail.com>
2023-03-30 21:26:23 +0800

    nx609j: Drop unneeded amplifier config file
    Change-Id: If089383571dd4bed2bc1f0800a209577c1d63d4e

--- device.mk
+++ device.mk
@@ -33,8 +33,7 @@ $(call inherit-product, device/nubia/msm8998-common/msm8998.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
-    $(LOCAL_PATH)/audio/speaker.ftcfg:$(TARGET_COPY_OUT_VENDOR)/etc/speaker.ftcfg
+    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 1920
